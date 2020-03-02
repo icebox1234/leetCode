f7bef11dd263862f472df1462c57d00a6411e8d1
icebox1234 <464752812@qq.com>
2020-03-02 23:18:08 +0800

    两数之和

--- code.js
+++ code.js
@@ -1,43 +1,14 @@
-class TreeNode {
-    constructor(val) {
-        this.val = val;
-        this.left = this.right = null;
+let twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; ++i) {
+        let temp = target - nums[i];
+        let found = nums.findIndex(item => item === temp);
+        if (found !== -1 && found !== i) {
+            return [i, found];
+        }
     }
-}
-
-function createTree(arr) {
-    if (arr.length <= 0) {
-        return null;
-    }
-    if (arr.length === 1) {
-        return new TreeNode(arr[0]);
-    }
-    return createTreeFn(arr, 0);
-}
-
-function createTreeFn(arr, rootIndex) {
-    debugger
-    let node = new TreeNode(arr[rootIndex]);
-    if (arr[2 * rootIndex + 1]) {
-        node.left = createTreeFn(arr, 2 * rootIndex + 1)
-    }
-    if (arr[2 * rootIndex + 2]) {
-        node.right = createTreeFn(arr, 2 * rootIndex + 2);
-    }
-    return node;
-}
-
-let mirrorTree = function (root) {
-    if (root === null) {
-        return null;
-    }
-    [root.left, root.right] = [root.right, root.left];
-    mirrorTree(root.left);
-    mirrorTree(root.right);
-    return root
 };
 
 function main() {
-    console.log(createTree([4, 2, 7, 1, 3, 6, 9]));
+    console.log(twoSum([2, 7, 11, 15], 9));
     // document.querySelector('#testCode').innerHTML = mirrorTree('#testCode');
 }
